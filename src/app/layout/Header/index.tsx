79cf6c4c5ee5fc77d82111458f80a474a8acd363
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-13 20:30:51 +0300

    feat: handle header styles dynamically on scroll

--- src/app/layout/Header/index.tsx
+++ src/app/layout/Header/index.tsx
@@ -1,10 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
 import SearchBar from "./SearchBar";
 
 const Header = () => {
+  const [isHeaderScrolled, setIsHeaderScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsHeaderScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="sticky z-50 top-0 left-0 w-full py-5">
+    <header
+      className={`sticky z-50 top-0 left-0 w-full transition-all duration-300 
+        ${isHeaderScrolled ? "bg-[#182026] py-3 shadow-sm shadow-surface" : "bg-transparent py-5"}`}
+    >
       <div className="container flex items-center justify-between">
         <Logo />
 
