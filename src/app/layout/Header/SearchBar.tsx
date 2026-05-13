d121570b2b6b5735d1c8b861a6db5af3d5877d4d
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-13 22:44:14 +0300

    feat: add search bar to header

--- src/app/layout/Header/SearchBar.tsx
+++ src/app/layout/Header/SearchBar.tsx
@@ -1,5 +1,31 @@
+"use client";
+
+import { useState } from "react";
+import { Search } from "lucide-react";
+
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [searchedMovie, setSearchedMovie] = useState<string>("");
+
+  return (
+    <div className="relative w-full">
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Search Movies..."
+        className="w-full md:w-50 ring-2 ring-muted md:ring-0 glass py-2 px-4 rounded-full transition-all duration-300 md:focus:w-64 focus:outline-none caret-primary text-sm focus:ring-2 focus:ring-primary/80 focus:shadow-md focus:shadow-primary"
+        value={searchedMovie}
+        onChange={(e) => setSearchedMovie(e.target.value)}
+      />
+
+      <label
+        htmlFor="search"
+        className="absolute inset-e-4 top-1/2 -translate-y-1/2 cursor-pointer"
+      >
+        <Search size={20} />
+      </label>
+    </div>
+  );
 };
 
 export default SearchBar;
