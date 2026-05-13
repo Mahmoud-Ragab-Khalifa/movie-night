47788bd5b952cf9037fdc3d05ca1dfa096fc6f8a
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-13 17:54:45 +0300

    feat: add the header structure

--- src/app/layout/Header/index.tsx
+++ src/app/layout/Header/index.tsx
@@ -1,10 +1,16 @@
 import Logo from "./Logo";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 
 const Header = () => {
   return (
     <header className="py-4">
       <div className="container flex items-center justify-between">
         <Logo />
+
+        <Navbar />
+
+        <SearchBar />
       </div>
     </header>
   );
