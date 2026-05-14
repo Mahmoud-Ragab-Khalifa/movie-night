bbb1591997e1e5ae49a452e294f84315afb2d783
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-14 03:59:38 +0300

    refactor: move fullscreen styles from home page to hero section

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,5 +1,7 @@
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="min-h-screen pt-19.5 md:pt-20 lg:pt-21.5">Hero</section>
+  );
 };
 
 export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./_components/Hero";
 
 const HomePage = () => {
   return (
-    <main className="min-h-screen pt-19.5 md:pt-20 lg:pt-21.5">
+    <main>
       <Hero />
     </main>
   );
