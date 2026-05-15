02f1bbedcaa07604a2a156f63e170cad835e3695
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-16 01:50:24 +0300

    feat: add get-popular-movies service in api services file

--- src/services/api.ts
+++ src/services/api.ts
@@ -24,3 +24,24 @@ export const getTrendingMovies = async () => {
 
   return trendingMovies.results ?? [];
 };
+
+export const getPopularMovies = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/movie/popular`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+      },
+    },
+  );
+
+  if (!response.ok) {
+    console.error("Failed To Get Popular Movies");
+  }
+
+  const popularMovies: TmdbResponse = await response.json();
+
+  return popularMovies.results ?? [];
+};
