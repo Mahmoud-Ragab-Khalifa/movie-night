598c0e851288c7a02f1be7420c9bd4ca783906fd
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-18 18:59:23 +0300

    feat: add footer links constants data

---
+++ src/constants/footerLinks.ts
@@ -0,0 +1,17 @@
+import { NavigationLinkItem } from "@/types/navigationLinkItem";
+
+export const quickLinks: NavigationLinkItem[] = [
+  { href: "#home", label: "Home" },
+  { href: "#trending", label: "Trending" },
+  { href: "#popular", label: "Popular" },
+  { href: "#top-rated", label: "Top Rated" },
+  { href: "#browse-by-genre", label: "Browse By Genre" },
+];
+
+export const resourcesLinks: NavigationLinkItem[] = [
+  { href: "#home", label: "About Us" },
+  { href: "#home", label: "Contact" },
+  { href: "#home", label: "Blog" },
+  { href: "#home", label: "FAQ" },
+  { href: "#home", label: "Help Center" },
+];
