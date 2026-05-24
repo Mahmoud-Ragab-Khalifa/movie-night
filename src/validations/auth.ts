c580f166b514574c36344b38b923db4b154874c4
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-25 02:55:17 +0300

    feat: add sign-up-schema to validate sign-up form

---
+++ src/validations/auth.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const signUpSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: "Username Is Required" }),
+
+    email: z.string().trim().email({ message: "Email Adderess Is Required" }),
+
+    password: z
+      .string()
+      .min(6, { message: "Password Must Be More Than 6 Characters" })
+      .max(40, { message: "Password Must Be Less Than 40 Characters" }),
+
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Password Must Be More Than 6 Characters" }),
+  })
+
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords Are Not The Same",
+    path: ["confirmPassword"],
+  });
