b5bd960bfd5e9b754ecd1479f03961b2f31e62b0
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-24 20:16:23 +0300

    feat: add the structure of profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+const ProfilePage = () => {
+  return (
+    <main className="min-h-dvh pt-19.5 md:pt-20 lg:pt-21.5">
+      <section className="section-gap">
+        <div className="container">Profile Page</div>
+      </section>
+    </main>
+  );
+};
+
+export default ProfilePage;
