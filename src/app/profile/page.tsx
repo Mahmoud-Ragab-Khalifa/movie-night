1cd945996ec9f6f01562a84609aba6f9f405ed9a
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-28 19:39:14 +0300

    feat: add profile data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,44 @@
+import { createSupabaseServerClient } from "@/lib/supabase/server-client";
+
 const ProfilePage = async () => {
-  return <section>Profile</section>;
+  const supabase = await createSupabaseServerClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  console.log(user);
+
+  return (
+    <section className="flex flex-col gap-5">
+      <div className="grid gap-0.5">
+        <span className="text-sm text-muted-foreground">Email:</span>
+        <span className="text-lg text-foreground">{user?.email}</span>
+      </div>
+
+      {user?.user_metadata.userName && (
+        <div className="grid gap-0.5">
+          <span className="text-sm text-muted-foreground">UserName:</span>
+          <span className="text-lg text-foreground">
+            {user?.user_metadata.userName}
+          </span>
+        </div>
+      )}
+
+      {user?.phone && (
+        <div className="grid gap-0.5">
+          <span className="text-sm text-muted-foreground">Phone:</span>
+          <span className="text-lg text-foreground">{user?.phone}</span>
+        </div>
+      )}
+
+      <div className="grid gap-0.5">
+        <span className="text-sm text-muted-foreground">Sign In With:</span>
+        <span className="text-lg text-foreground capitalize">
+          {user?.app_metadata.provider}
+        </span>
+      </div>
+    </section>
+  );
 };
 
 export default ProfilePage;
