0936616ebb145780d7831008588f5f1df013cbeb
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-29 22:26:32 +0300

    feat: add zod validation schema of update-profile form

---
+++ src/validations/updateProfile.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const updateProfileSchema = z.object({
+  userName: z.string().trim().min(1, { message: "User Name Is Required" }),
+  email: z.string().trim().email({ message: "Email Is Required" }),
+  fullName: z.string().trim().optional(),
+  phone: z.string().trim().optional(),
+  bio: z.string().trim().optional(),
+  image: z.string().trim().optional(),
+});
