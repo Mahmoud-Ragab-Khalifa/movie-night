376ba431edf0c759d1dd3109a6e3a2f5e9ee0905
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-30 23:58:52 +0300

    feat: add watch-list-movie type

---
+++ src/types/watchListMovie.ts
@@ -0,0 +1,10 @@
+export type WatchListMovie = {
+  id: number;
+  user_id: string;
+  movie_id: number;
+  image: string;
+  title: string;
+  release_date: number;
+  rating: number;
+  created_at: string;
+};
