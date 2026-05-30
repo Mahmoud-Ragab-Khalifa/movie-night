a7dac79676938508cb08ddb4630627c55b0f605f
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-31 00:32:22 +0300

    style: add fade-in animation to main profile data blocks

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -16,13 +16,13 @@ const ProfilePage = async () => {
   return (
     <section className="flex flex-col-reverse lg:flex-row gap-5 items-start lg:gap-20 xl:gap-30 flex-1 w-full pb-8 md:pb-0">
       <div className="flex flex-col gap-5">
-        <div className="grid gap-0.5">
+        <div className="grid gap-0.5 animate-fade-in-lg">
           <span className="text-sm text-muted-foreground">Email:</span>
           <span className="text-lg text-foreground">{userProfile.email}</span>
         </div>
 
         {userProfile.user_name && (
-          <div className="grid gap-0.5">
+          <div className="grid gap-0.5 animate-fade-in-lg animation-delay-100">
             <span className="text-sm text-muted-foreground">User Name:</span>
             <span className="text-lg text-foreground">
               {userProfile.user_name}
@@ -31,7 +31,7 @@ const ProfilePage = async () => {
         )}
 
         {userProfile.full_name && (
-          <div className="grid gap-0.5">
+          <div className="grid gap-0.5 animate-fade-in-lg animation-delay-200">
             <span className="text-sm text-muted-foreground">Full Name:</span>
             <span className="text-lg text-foreground">
               {userProfile.full_name}
@@ -40,20 +40,20 @@ const ProfilePage = async () => {
         )}
 
         {userProfile.bio && (
-          <div className="grid gap-0.5">
+          <div className="grid gap-0.5 animate-fade-in-lg animation-delay-300">
             <span className="text-sm text-muted-foreground">Bio:</span>
             <span className="text-lg text-foreground">{userProfile.bio}</span>
           </div>
         )}
 
         {userProfile.phone && (
-          <div className="grid gap-0.5">
+          <div className="grid gap-0.5 animate-fade-in-lg animation-delay-400">
             <span className="text-sm text-muted-foreground">Phone:</span>
             <span className="text-lg text-foreground">{userProfile.phone}</span>
           </div>
         )}
 
-        <div className="grid gap-0.5">
+        <div className="grid gap-0.5 animate-fade-in-lg animation-delay-500">
           <span className="text-sm text-muted-foreground">Sign In With:</span>
           <span className="text-lg text-foreground capitalize">
             {user?.app_metadata.provider}
@@ -62,7 +62,7 @@ const ProfilePage = async () => {
       </div>
 
       {userProfile.image_url && (
-        <div className="relative w-50 h-50 rounded-full overflow-hidden mx-auto md:mx-0">
+        <div className="relative w-50 h-50 rounded-full overflow-hidden mx-auto md:mx-0 animate-fade-in-lg animation-delay-600">
           <Image
             src={userProfile.image_url}
             alt={userProfile.id}
