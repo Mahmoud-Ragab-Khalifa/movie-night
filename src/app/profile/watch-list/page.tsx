589734a1b4b7b7ad727f438e65b00f5991d11914
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-31 00:21:49 +0300

    refactor: add fallback text and CTA link to home page if user don't have any movies in its watchlist to add some

--- src/app/profile/watch-list/page.tsx
+++ src/app/profile/watch-list/page.tsx
@@ -12,38 +12,50 @@ const WatchListPage = async () => {
 
   return (
     <section className="flex-1 w-full space-y-5">
-      {watchListMovies!.map((movie, idx) => (
-        <Link
-          key={idx}
-          href={`/movie/${movie.movie_id}/details`}
-          className={`glass-strong rounded-lg transition-all duration-500 active:scale-110 hover:ring-2 hover:ring-primary/60 flex items-center gap-5 p-4 animate-fade-in-lg ${idx < 10 && idx !== 0 && `animation-delay-${idx}00`}`}
-        >
-          <div className="relative w-25 h-25 rounded-lg overflow-hidden">
-            <Image
-              src={getImageUrl(
-                ImageSizes.W154,
-                movie.image,
-                ImageTypes.MOVIE_CARD,
-              )}
-              alt={movie.title}
-              fill
-              sizes="100px"
-              className="object-cover object-center"
-            />
-          </div>
-
-          <div className="flex flex-col gap-1">
-            <h1>{movie.title}</h1>
-            <span className="text-sm text-muted-foreground">
-              Release Date: {movie.release_date}
-            </span>
-            <div className="flex items-center gap-1.5 text-sm">
-              <StarIcon className="text-yellow-500 fill-yellow-500" size={17} />
-              <span className="mt-0.5 block">{movie.rating}</span>
+      {watchListMovies && watchListMovies.length > 0 ? (
+        watchListMovies.map((movie, idx) => (
+          <Link
+            key={idx}
+            href={`/movie/${movie.movie_id}/details`}
+            className={`glass-strong rounded-lg transition-all duration-500 active:scale-110 hover:ring-2 hover:ring-primary/60 flex items-center gap-5 p-4 animate-fade-in-lg ${idx < 10 && idx !== 0 && `animation-delay-${idx}00`}`}
+          >
+            <div className="relative w-25 h-25 rounded-lg overflow-hidden">
+              <Image
+                src={getImageUrl(
+                  ImageSizes.W154,
+                  movie.image,
+                  ImageTypes.MOVIE_CARD,
+                )}
+                alt={movie.title}
+                fill
+                sizes="100px"
+                className="object-cover object-center"
+              />
             </div>
-          </div>
-        </Link>
-      ))}
+
+            <div className="flex flex-col gap-1">
+              <h1>{movie.title}</h1>
+              <span className="text-sm text-muted-foreground">
+                Release Date: {movie.release_date}
+              </span>
+              <div className="flex items-center gap-1.5 text-sm">
+                <StarIcon
+                  className="text-yellow-500 fill-yellow-500"
+                  size={17}
+                />
+                <span className="mt-0.5 block">{movie.rating}</span>
+              </div>
+            </div>
+          </Link>
+        ))
+      ) : (
+        <p className="text-center text-muted-foreground text-sm italic">
+          No Movies Found In Your WatchList{" "}
+          <Link href={"/"} className="text-primary font-bold animate-pulse">
+            Start Browse Movies
+          </Link>
+        </p>
+      )}
     </section>
   );
 };
